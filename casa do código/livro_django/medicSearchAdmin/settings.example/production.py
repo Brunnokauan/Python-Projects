128d96ad2207781d90e4cfd1d3655db4df9ff59b
Brunno Kauan Duarte de Goes <X382450@rede.sp>
2023-01-11 16:06:11 -0300

    Add projeto

---
+++ casa do código/livro_django/medicSearchAdmin/settings.example/production.py
@@ -0,0 +1,14 @@
+from .settings import *
+
+DEBUG = False
+
+SECRET_KEY = ''
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
