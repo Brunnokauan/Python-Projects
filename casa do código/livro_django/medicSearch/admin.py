eb0ca498e2656ccedc6155452dab749940c8e910
Brunno Kauan Duarte de Goes <X382450@rede.sp>
2023-01-13 14:52:34 -0300

    Customização da admin

--- casa do código/livro_django/medicSearch/admin.py
+++ casa do código/livro_django/medicSearch/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import *
 
+class PofileAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)
